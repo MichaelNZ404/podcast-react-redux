571770d29665addb605b3ac486ee18598279d66b
Michael Coleman <michael.coleman404@gmail.com>
2018-02-17 19:25:06 +0000

    Added alot of boilerplate to allow redux devtools, and set up redux action, reducers, containers and store. Disabled some of the more annoying tslinting rules. Installed missing enzyme adapter dependency and connected this in setup tests file.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
 import './index.css';
+import Player from './containers/Player';
+
+const initialState = {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+}
+
+const store = createStore<StoreState>(
+  enthusiasm, initialState,
+  window['devToolsExtension'] ? window['devToolsExtension']() : createStore);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Player />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
